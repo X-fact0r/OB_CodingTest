e9bb592882a5d0b9a62ab501b8c15b694603f038
X-fact0r <X-fact0r@users.noreply.github.com>
2016-11-10 23:00:36 +0000

    Implementing javascript string-extensions
    Implemented string formatting extensions, tests all passing code could be
    refactored to make more consistant

--- JasmineTests/scripts/string-extensions.js
+++ JasmineTests/scripts/string-extensions.js
@@ -1,4 +1,34 @@
-﻿String.prototype.capitalise = function () {}
-String.prototype.camelCaseToSpineCase = function () { }
-String.prototype.spineCaseToCamelCase = function () { }
-String.prototype.format = function () { }
+﻿if (typeof String.prototype.capitalise !== "function") {
+    String.prototype.capitalise = function () {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+}
+
+if (typeof String.prototype.camelCaseToSpineCase !== "function") {
+    String.prototype.camelCaseToSpineCase = function () {
+        return this.replace(/[-]/g, ' ') // switch dashes to spaces
+                   .replace(/(?!^)([A-Z])/g, ' $1')
+                   .replace(/[_\s]+(?=[a-zA-Z])/g, '-').toLowerCase()
+    }
+}
+
+if (typeof String.prototype.spineCaseToCamelCase !== "function") {
+    String.prototype.spineCaseToCamelCase = function () {
+        return this.trim()
+            .replace(/\s+/g, ' ')
+            .replace(/[ ]/g, '-')
+                   .replace(/(\-\w)/g, function (m) { return m[1].toUpperCase(); });
+    }
+}
+
+if (typeof String.prototype.format !== "function") {
+    String.prototype.format = function () {
+        var args = arguments;
+        return this.replace(/{(\d+)}/g, function (match, number) {
+            return typeof args[number] != 'undefined'
+              ? args[number]
+              : match
+            ;
+        });
+    }
+}
